8e7f78f03b20dc6359c1590486256393da6aaebf
Egor <DronovEgorVl@yandex.com>
2021-06-03 14:11:51 +0300

    Key.java (gitignore)

--- src/main/java/Key.java
+++
@@ -1,8 +0,0 @@
-public class Key {
-    protected static String getKey() {
-        return "1795632261:AAFrv-fYTjxCXo5Fnoxzc_w500eChd76MZk";
-    }
-    protected static String getName() {
-        return "TestSofartsTelegramBot";
-    }
-}
