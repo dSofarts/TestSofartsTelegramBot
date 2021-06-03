19ed5fa18ad68ba425ad6fdbea9c680e5047d938
Egor <DronovEgorVl@yandex.com>
2021-06-03 14:06:40 +0300

    Modifying a file Bot.java and created Key.java (gitignore)

---
+++ src/main/java/Key.java
@@ -0,0 +1,8 @@
+public class Key {
+    protected static String getKey() {
+        return "1795632261:AAFrv-fYTjxCXo5Fnoxzc_w500eChd76MZk";
+    }
+    protected static String getName() {
+        return "TestSofartsTelegramBot";
+    }
+}
