28cc5dee4b98eb0547df726d63099ee7bc6cb11f
Egor <DronovEgorVl@yandex.com>
2021-06-03 14:05:50 +0300

    Modifying a file Bot.java and created Key.java (gitignore)

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,2 +1,29 @@
-public class Bot {
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return Key.getName();
+    }
+
+    public String getBotToken() {
+        return Key.getKey();
+    }
 }
