9abe67950a18a8dd02d9f4ef506c5660766b83fe
Egor <DronovEgorVl@yandex.com>
2021-06-03 14:47:00 +0300

    Modifying a file Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,22 +1,50 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 public class Bot extends TelegramLongPollingBot {
+
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
             telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
         } catch (TelegramApiException ex) {
             ex.printStackTrace();
         }
     }
 
     public void onUpdateReceived(Update update) {
-
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/setting":
+                    sendMsg(message, "Что будем настраивать?");
+                    break;
+                default:
+            }
+        }
     }
 
     public String getBotUsername() {
